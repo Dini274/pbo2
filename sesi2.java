b3b98e266c6244193979f7a12662c1b91e447a70
Dini274 <77830581+Dini274@users.noreply.github.com>
2021-09-28 21:57:26 +0700

    Add files via upload

---
+++ sesi2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ternary2;
+
+/**
+ *
+ * @author dini aryani
+ */
+public class sesi2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        
+        int num1 = 50000;
+        int num2 = 55000;
+        
+        String result = num1 >= num2 ? "membeli":"tidak membeli";
+        
+        System.out.println(result);
+    }
+    
+}
